e647dcd77791906376f2678e2e9a28c4fbb935c5
Thiago NR <thiagokroos55gmail.com>
2023-09-01 22:48:29 -0300

    adicionando

--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -1,20 +1,19 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subscriber, subscribeOn } from 'rxjs';
 import { PokemonsService } from 'src/app/services/pokemons.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent {
-  dadosDaAPI = 'https://pokeapi.co/api/v2/pokemon/'
-constructor(private pokemonsService: PokemonsService){}
-ngOnInit(): void{
-this.pokemonsService.ListaPokemons().subscribe(pokemonsService => console.log(pokemonsService))
-
-}
-
-
+  dadosPai = [];
+  constructor(private pokemonsService: PokemonsService) {}
+  ngOnInit(): void {
+    this.pokemonsService.ListaPokemons().subscribe((response) => {
+      this.dadosPai;
+    });
+  }
 }
--- src/app/listas/card-list/card-list.component.ts
+++ src/app/listas/card-list/card-list.component.ts
@@ -4,15 +4,15 @@ import { PokemonsService } from 'src/app/services/pokemons.service';
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.css']
+  styleUrls: ['./card-list.component.css'],
 })
 export class CardListComponent {
   @Input()
-  dadosAPI: any[] = [];
+  dadosFilho: any[] = [];
 
   constructor(private pokemonsService: PokemonsService){}
 ngOnInit(): void{
-this.dadosAPI
+this.dadosFilho
 
 }
 
